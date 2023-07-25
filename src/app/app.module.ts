47a1cb2196d5f9817355e8164066c165cfdcb152
arun <arun.bs@succeedlearn.com>
2023-07-25 20:21:47 +0530

    image deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageComponent
   ],
   imports: [
     BrowserModule
